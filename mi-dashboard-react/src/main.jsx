bbdff930eb6cdc273629a6d374c573fbe2a1ff89
AndresDeJesus <adjzp@gmail.com>
2025-08-17 13:44:22 -0400

    Agregando-ServiceWorker-para-gestionar-el-cache-de-la-aplicacion-y-poder-usarla-sin-conexion-a-internet

--- mi-dashboard-react/src/main.jsx
+++ mi-dashboard-react/src/main.jsx
@@ -4,6 +4,14 @@ import App from './App.jsx';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css'; 
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => console.log('Service Worker registrado:', registration))
+      .catch(error => console.error('Fallo al registrar Service Worker:', error));
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <MantineProvider defaultColorScheme="dark">
